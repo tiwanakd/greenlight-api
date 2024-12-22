4a25b70f8462561372024cd3448e275b3d6b0c3a
tiwanakd <karandeep.tiwana@outlook.com>
2024-12-22 02:28:31 -0800

    added commit time to version

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -6,6 +6,7 @@ import (
 )
 
 func Version() string {
+	var time string
 	var revision string
 	var modified bool
 
@@ -13,6 +14,8 @@ func Version() string {
 	if ok {
 		for _, s := range bi.Settings {
 			switch s.Key {
+			case "vcs.time":
+				time = s.Value
 			case "vcs.revision":
 				revision = s.Value
 			case "vcs.modified":
@@ -24,8 +27,8 @@ func Version() string {
 	}
 
 	if modified {
-		return fmt.Sprintf("%s-dirty", revision)
+		return fmt.Sprintf("%s-%s-dirty", time, revision)
 	}
 
-	return revision
+	return fmt.Sprintf("%s-%s", time, revision)
 }
